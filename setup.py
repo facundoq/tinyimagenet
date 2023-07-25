d374ce0d25c8008f267e175f6e7e0afc93eda186
facundoq <facundoq@gmail.com>
2023-07-25 00:06:47 -0300

    added tests to ensure dataset gets loaded properly

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 
 
 url="https://github.com/facundoq/tinyimagenet"
-VERSION="0.6"
+VERSION="0.7"
 
 class UploadCommand(Command):
     """Support setup.py upload."""
